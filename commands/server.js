d7485db952c2baaa704d2eda5f8a3639109eea7b
SahrulDXD <sahrulgnwn385@gmail.com>
2021-02-25 16:03:09 +0700

    test

---
+++ commands/server.js
@@ -0,0 +1,15 @@
+/**
+ * This class responds to anyone that types !bot talk and chooses one of the phrases below to respond with at random.
+ *
+ */
+module.exports = {
+    name: 'server', // The name of the command
+    description: 'Server Control', // The description of the command (for help text)
+    args: true, // Specified that this command doesn't need any data other than the command
+    usage: '', // Help text to explain how to use the command (if it had any arguments)
+    execute(message, args) {
+        if(args[0] == "test") {
+            message.channel.send("TEST")
+        }
+    },
+};

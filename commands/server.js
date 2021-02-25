3acfd7f67378012eb0a599301c747ae51d0093cf
SahrulDXD <sahrulgnwn385@gmail.com>
2021-02-25 16:55:24 +0700

    Update server.js

--- commands/server.js
+++ commands/server.js
@@ -4,7 +4,8 @@
  */
 const { prefix, token, host, key } = require('../config.json');     // Loads the "token" and "prefix" values from the config file
 const node = require('nodeactyl-beta');
-
+const node2 = require('nodeactyl-v1-support')
+const Admin = node2.Admin;
 module.exports = {
     name: 'server', // The name of the command
     description: 'Server Control', // The description of the command (for help text)
@@ -12,8 +13,14 @@ module.exports = {
     usage: '', // Help text to explain how to use the command (if it had any arguments)
     async execute(message, args) {
         if(args[0] == "test") {
-            const Application = new node.NodeactylApplication(host, key);
-            let server = await Application.getAllServers();
+            Admin.login(host, key, (logged_in, err) => {
+                console.log(logged_in);
+                /** If you want call the function in here, 
+                 * But we prefer you do have Application.login() at the top of your
+                 * project and use the following syntax:
+                 */
+            });
+            let server = await Admin.getAllServers();
             for(let i = 0; i< server.length; i++) {
                 let name = server[i].attributes.name;
                 let id = server[i].attributes.id;

5ce033202dbdb702a0cde2f6d6dc5c424d8241b5
SahrulDXD <sahrulgnwn385@gmail.com>
2021-02-25 16:24:24 +0700

    Update server.js

--- commands/server.js
+++ commands/server.js
@@ -2,7 +2,7 @@
  * This class responds to anyone that types !bot talk and chooses one of the phrases below to respond with at random.
  *
  */
-const { prefix, token, host, key } = require('./config.json');     // Loads the "token" and "prefix" values from the config file
+const { prefix, token, host, key } = require('..//config.json');     // Loads the "token" and "prefix" values from the config file
 const node = require('nodeactyl-beta');
 
 module.exports = {
@@ -10,7 +10,7 @@ module.exports = {
     description: 'Server Control', // The description of the command (for help text)
     args: true, // Specified that this command doesn't need any data other than the command
     usage: '', // Help text to explain how to use the command (if it had any arguments)
-    execute(message, args) {
+    async execute(message, args) {
         if(args[0] == "test") {
             const Application = new node.NodeactylApplication(host, key);
             Application.getAllServers().then(servers => {

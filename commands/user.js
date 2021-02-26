9d12b6f29b7154c1feb7f20c27433040bada2a08
SahrulDXD <sahrulgnwn385@gmail.com>
2021-02-26 21:16:54 +0700

    Update user.js

--- commands/user.js
+++ commands/user.js
@@ -20,44 +20,47 @@ module.exports = {
         if (message.member.hasPermission(["MANAGE_GUILD", "ADMINISTRATOR"])) {
             if (args[0] == "create"){
                 let allUser = await Application.getAllUsers();
-                let isUserExist = false;
+                let isEmailExist = false;
+                let isUsernameExist = false;
                 console.log(allUser.data);
-                message.channel.send("ERROR COK");
                 for(let i = 0; i< allUser.data.length; i++){
-                    console.log(allUser.data[i].attributes.username);
-                    console.log(allUser.data[i].attributes.email);
+                    // console.log(allUser.data[i].attributes.username);
+                    // console.log(allUser.data[i].attributes.email);
                     if (allUser.data[i].attributes.email == args[1]){
-                        isUserExist = true;
+                        isEmailExist = true;
+                        break;
+                    }
+                    if (allUser.data[i].attributes.username == args[2]) {
+                        isUsernameExist = true;
                         break;
                     }
                 }
-                if (isUserExist){
-                    message.channel.send("ERROR COK");
-                    return;
+                if (isEmailExist){
+                    message.channel.send("Email Sudah Ada");
+                    return; //kalo belum ada lanjut teros
                 }
-                console.log("masuk sini");
-                // let user = await Application.createUser(args[1], args[2], args[3], args[4]);
-                // if(!user){
-                //     message.channel.send("ERROR COK")
-                // } else {
-                //     let userid = user.attributes.id;
-                //     let username = user.attributes.username;
-                //     let email = user.attributes.email;
-                //     let first_name = user.attributes.first_name;
-                //     let last_name = user.attributes.last_name;
-                //     // Application.createUser("kaguvps@gmail.com", "KaguVPS", "Kagu", "Vps").then(user => {
-                //     let UserInfo = new MessageEmbed()
-                //         .setAuthor("Account Created", "https://cdn.discordapp.com/attachments/786854213916426240/814077727534612500/depositphotos_81700460-stock-illustration-monogram-l-logo-letter.jpg")
-                //         .setColor("GREEN")
-                //         .setTitle(`${username} (${userid})`)
-                //         .setDescription("Akun Kamu kamu berhasil di buat, Periksa Email Untuk Mengubah Password, Periksa Pada Bagian Promosi Jika Email Tidak Muncul")
-                //         .addField("Username", username)
-                //         .addField("Email", email)
-                //         .addField("First Name", first_name, true)
-                //         .addField("Last Name", last_name, true)
-                //         .setFooter("Leyz Store", "https://cdn.discordapp.com/attachments/786854213916426240/814077727534612500/depositphotos_81700460-stock-illustration-monogram-l-logo-letter.jpg")
-                //     message.channel.send(UserInfo)
-                // }
+                if (isUsernameExist){
+                    message.channel.send("Username Sudah Ada");
+                    return; // berhenti disini
+                }
+                let user = await Application.createUser(args[1], args[2], args[3], args[4]);
+                let userid = user.attributes.id;
+                let username = user.attributes.username;
+                let email = user.attributes.email;
+                let first_name = user.attributes.first_name;
+                let last_name = user.attributes.last_name;
+                // Application.createUser("kaguvps@gmail.com", "KaguVPS", "Kagu", "Vps").then(user => {
+                let UserInfo = new MessageEmbed()
+                    .setAuthor("Account Created", "https://cdn.discordapp.com/attachments/786854213916426240/814077727534612500/depositphotos_81700460-stock-illustration-monogram-l-logo-letter.jpg")
+                    .setColor("GREEN")
+                    .setTitle(`${username} (${userid})`)
+                    .setDescription("Akun Kamu kamu berhasil di buat, Periksa Email Untuk Mengubah Password, Periksa Pada Bagian Promosi Jika Email Tidak Muncul")
+                    .addField("Username", username)
+                    .addField("Email", email)
+                    .addField("First Name", first_name, true)
+                    .addField("Last Name", last_name, true)
+                    .setFooter("Leyz Store", "https://cdn.discordapp.com/attachments/786854213916426240/814077727534612500/depositphotos_81700460-stock-illustration-monogram-l-logo-letter.jpg")
+                message.channel.send(UserInfo)
             }
         }
     },
